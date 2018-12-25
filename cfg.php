2927e07cf047d86ead8285b0e9e95b8e504b3cdd
f0xyk <46134813+f0xyk@users.noreply.github.com>
2018-12-25 23:50:10 +0800

    Delete cfg.php

--- cfg.php
+++
@@ -1,7 +0,0 @@
-<?php
-//ikuti format config di bawah "ticket=tiket_kamu&meid=meid_kamu",
-//jangan ada sepasi
-$config = [
-"ticket=sHp2r312otmCpnGns3mCqJhovNiBh5rRr-CKobW1h8-9iphpfqzElIOpfaq1eHWqhLCozoqdh9u60J9twKWc262jra6KZM3XjZSKo7-Ik2ubfrCUi4RzoQ&meid=1d850e6551f95303",
-  ];
-?>
